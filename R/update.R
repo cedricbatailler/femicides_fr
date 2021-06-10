69c6457f93cbc17362571b86fd61982b3a5540eb
Cedric Batailler <cedric.batailler@gmail.com>
2021-06-10 11:53:13 +0200

    Initial commit

---
+++ R/update.R
@@ -0,0 +1,51 @@
+# packages --------------------------------------------------------------------
+library(rtweet)
+library(lubridate)
+library(glue)
+library(stringr)
+library(dplyr)
+
+# twitter token ---------------------------------------------------------------
+femicide_token <- 
+  rtweet::create_token(
+    app = "femicide-fr-datset", 
+    consumer_key    = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+    consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+    access_token    = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+    access_secret   = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+    set_renv = FALSE
+  )
+ 
+# params ----------------------------------------------------------------------
+twitter_handle <- "feminicidesfr"
+last_tweets_n  <- 150
+
+# clean last tweets  ----------------------------------------------------------
+# get last tweets
+tweets <-
+  rtweet::get_timeline(twitter_handle, n = last_tweets_n, 
+                       token = femicide_token) 
+
+femicide_timeline <-
+  tweets %>% 
+  dplyr::filter(stringr::str_detect(text,  "^\\[\\d+")) %>% 
+  dplyr::mutate(tweet_id   = status_id,
+         tweet_datetime = created_at,
+         tweet_text = text,
+         tweet_urls = urls_expanded_url,
+         tweet_text_markup     = stringr::str_extract(text, "(?<=^\\[).*(?=\\])"), 
+         year                  = stringr::str_extract(tweet_text_markup, "\\d{4}"),
+         femicide              = stringr::str_extract(tweet_text_markup, "^\\d+"),
+         .keep = "none") %>% 
+  dplyr::mutate(across(c(year, femicide, tweet_id),
+                ~ as.numeric(.))) %>% 
+  dplyr::mutate(year = ifelse(is.na(year), lubridate::year(tweet_datetime), year)) %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(tweet_urls = glue::glue_collapse(tweet_urls, sep = " ")) %>% 
+  dplyr::ungroup()
+
+# update csv data set ---------------------------------------------------------
+readr::read_csv("data/femicide.csv") %>% 
+  dplyr::bind_rows(femicide_timeline) %>% 
+  dplyr::distinct() %>% 
+  readr::write_csv("data/femicide.csv")
---
+++ README.md
@@ -0,0 +1,9 @@
+# femicides_fr
+
+![workflow-update](https://github.com/cedricbatailler/femicides_fr/actions/workflows/femicide-tweets.yml/badge.svg)
+
+A data set of femicides in France as reported by [\@feminicidesfr](https://twitter.com/feminicidesfr/).
+
+## Similar projects
+
+-   [2021 - Féminicides par Compagnons ou Ex - Google My Maps](https://www.google.com/maps/d/viewer?mid=1AoTHJTAvWz-P1ddCKe1NhWtdsOQgcLPA): An interactive map of femicide maintained by [\@feminicidesfr](https://twitter.com/feminicidesfr/).
